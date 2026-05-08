ff38c199c0ce75e59f6d1b54cd070839fc23c410
caesar397 <korneluk.gabriela@gmail.com>
2026-05-08 13:08:13 +0200

    chore: refactoring project structure to mvc

---
+++ mapbook_lib/model.py
@@ -0,0 +1,16 @@
+users = [{
+    "name": "Gabriela",
+    "location": "Lublin",
+    "posts": ["Sprzedam Opla", "Jak wyciąć katalizator?", "Co oznacza czerwona kotrolka?"]
+    },
+    {
+        "name": "Daniel",
+        "location": "Legionowo",
+        "posts": ["Jak obliczyć współrzędne z gwiad?", "Ładna pogoda"]
+    },
+    {
+        "name": "Kamil",
+        "location": "Ciechanów",
+        "posts": ["Czy ktoś już zrobił sprawozdanie z PPyt?"]
+    }
+]
